bdf1e7fafedf6cdcb98c4240157379adf24c6dda
IlyaReinhard <95580934+IlyaReinhard@users.noreply.github.com>
2022-04-18 13:05:16 +0300

    Update urls.py

--- iCrawler/urls.py
+++ iCrawler/urls.py
@@ -19,11 +19,7 @@ from rest_framework import routers
 from quickstart import views
 
 #router = routers.DefaultRouter()
-"""
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'quotes', views.QueryViewSet)
-"""
+
 
 
 
